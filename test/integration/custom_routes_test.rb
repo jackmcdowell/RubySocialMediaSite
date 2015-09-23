8ee0c0fdaa4aee6baa8eac91a3940a64670cfd57
jackmcdowell <jackmcdowell@gmail.com>
2015-09-22 21:59:22 -0700

    updated profile page and scoped status updates

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -6,13 +6,20 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
 		assert_response :success
 	end
 
-		test "that /logout route logs out" do
+	test "that /logout route logs out" do
 		get '/logout'
 		assert_response :redirect
 		assert_redirected_to '/'
 	end
-		test "that /register route opens the sign up page" do
+	
+	test "that /register route opens the sign up page" do
 		get '/register'
 		assert_response :success
 	end
+
+	test "that a profile page works" do
+		get '/jackmcdowell'
+		assert_response :success
+	end
+
 end
